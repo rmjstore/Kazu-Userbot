8779e5d5928e34f76e5d2e4e1e352790f90ddf14
bluefloyd24 <gluhdb@gmail.com>
2024-11-11 04:21:54 +0700

    Update __init__.py

--- Stringyins/__init__.py
+++ Stringyins/__init__.py
@@ -1,2 +1,13 @@
 from .strings import get_languages, get_string, language
 from bs4 import BeautifulSoup
+import yaml
+import os
+
+# Memuat file YAML
+yaml_path = os.path.join(os.path.dirname(__file__), "id.yml")
+with open(yaml_path, "r", encoding="utf-8") as file:
+    strings = yaml.safe_load(file)
+
+# Fungsi untuk mengambil string berdasarkan kunci
+def get_string(key):
+    return strings.get(key, "")
