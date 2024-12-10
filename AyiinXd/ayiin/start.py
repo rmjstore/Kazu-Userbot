303e405b0a59f53f083367be965df3a6afb1e62a
bluefloyd24 <gluhdb@gmail.com>
2024-12-10 19:34:28 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -18,9 +18,9 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
-                caption="✨ **𝗕𝗟𝗨𝗘𝗙𝗟𝗢𝗬𝗗-Userbot Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 9.0@ALBY-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecek Bot**\n➠ **Ketik** `.help` **Untuk Melihat Informasi Module**\n━━━━━━━━━━━\n➠ **Powered By:** @ruangprojects ",
-                buttons=[(Button.url("ɢʀᴏᴜᴘ ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ruangdiskusikami"),)],
+                "https://telegra.ph/file/78fbd9d73e1f456857222.jpg",
+                caption="𝗕𝗟𝗨𝗘𝗙𝗟𝗢𝗬𝗗-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                buttons=[(Button.url("Channel", "https://t.me/bluefloydd"),)],
             )
     except Exception as e:
         LOGS.error(e)
