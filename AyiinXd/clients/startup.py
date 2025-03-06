64461ced7785828e02d58b9309f38179884f24c1
rmjst <devilatansyah@gmail.com>
2025-03-06 11:20:40 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -18,8 +18,8 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\n𝗜𝗫𝗔𝗟𝗟-Userbot {} Copyright © 2021-2025 IXALLUSERBOT• <https://github.com/ixally/Kazu-Userbot>"
-MSG_BLACKLIST = "𝗜𝗫𝗔𝗟𝗟-Userbot {}\nCopyright © 2021-2025 IXALLUSERBOT• <https://github.com/ixally/Kazu-Userbot>"
+EOL = "EOL\nrmj-Userbot {} Copyright © 2021-2025 IXALLUSERBOT• <https://github.com/ixally/Kazu-Userbot>"
+MSG_BLACKLIST = "rmj-Userbot {}\nCopyright © 2021-2025 IXALLUSERBOT• <https://github.com/ixally/Kazu-Userbot>"
 
 
 async def ayiin_client(client):
