8c8470a4f154edb73624ed1e71ea69961e880674
rmjst <devilatansyah@gmail.com>
2025-03-06 11:36:10 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -19,9 +19,9 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/b17248930f58bbdaf2e23.jpg",
-                caption="𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
-                buttons=[(Button.url("Store", "https://t.me/jasebxall")),
-                         (Button.url("Support", "https://t.me/ixallsupport"))]
+                caption="rmj-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                buttons=[(Button.url("Store", "https://t.me/roemahjaseb")),
+                         (Button.url("Support", "https://t.me/roemahjasebsupport"))]
             )
     except Exception as e:
         LOGS.error(e)
